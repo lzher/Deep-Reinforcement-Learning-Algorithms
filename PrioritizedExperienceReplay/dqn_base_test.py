10e90cdf45b1c5716c3571981ba745cc5cf1f8e3
lzhtony <lzhtony@foxmail.com>
2019-03-22 08:56:40 +0800

    average reward analysis awk; result data and figures.

--- PrioritizedExperienceReplay/dqn_base_test.py
+++ PrioritizedExperienceReplay/dqn_base_test.py
@@ -2,13 +2,19 @@ from dqn_base import DQN, ReplayMemory
 import gym
 import datetime
 import os
+from tqdm import tqdm
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import numpy as np
 
 env = gym.make('CartPole-v1').unwrapped
 
+N_TIMES = 5
 N_EP = 300
 
 N_WARMUP = 200
 N_REPLACE_TARGET = 200
+N_MAX_STEP = 2000
 
 N_MEM = 30000
 N_BATCH = 32
@@ -24,49 +30,60 @@ LOG_FILE = LOG_PATH + '/train.log'
 log_fp = open(LOG_FILE, 'w')
 print(LOG_FILE)
 
+save_steps = np.zeros((N_TIMES, N_EP))
+
 dqn = DQN(s_dim, a_val, a_dim)
 mem = ReplayMemory(s_dim, a_dim, N_MEM)
 
-total_step = 0
-train_step = 0
-for ep in range(N_EP):
-    ep_reward = 0
-    step = 0
-    state = env.reset()
-    while True:
-        # env.render()
-        action = dqn.choose_action(state)
-        state_next, reward, done, _ = env.step(action)
-        
-        xpos, xvel, theta, thetavel = state_next
-        r1 = (env.x_threshold - abs(xpos)) / env.x_threshold - 0.5
-        r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-        reward = r1 + r2
-        
-        mem.store_transition(state, state_next, action, reward)
-        
-        if total_step > N_WARMUP:
-            dqn.learn(mem, N_BATCH, replacement=(train_step % N_REPLACE_TARGET == 0))
-            train_step += 1
+for t in range(N_TIMES):
+    print("Time: %d" % t)
+    dqn.reset()
+    mem.reset()
+    total_step = 0
+    train_step = 0
+    for ep in tqdm(range(N_EP)):
+        ep_reward = 0
+        step = 0
+        state = env.reset()
+        while True:
+            # env.render()
+            action = dqn.choose_action(state)
+            state_next, reward, done, _ = env.step(action)
             
-        state = state_next
-        ep_reward += reward
-        step += 1
-        total_step += 1
-        
-        if done:
-            report = "E: {ep} S: {step} T: {ts} R: {reward}\n".format(
-                ep=ep,
-                step=step,
-                reward=ep_reward,
-                ts=total_step)
-            log_fp.write(report)
-            log_fp.flush()
-            print(report)
-            break
+            xpos, xvel, theta, thetavel = state_next
+            r1 = (env.x_threshold - abs(xpos)) / env.x_threshold - 0.5
+            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
+            reward = r1 + r2
+            
+            mem.store_transition(state, state_next, action, reward)
+            
+            if total_step > N_WARMUP:
+                dqn.learn(mem, N_BATCH, replacement=(train_step % N_REPLACE_TARGET == 0))
+                train_step += 1
+                
+            state = state_next
+            ep_reward += reward
+            step += 1
+            total_step += 1
+            
+            if done or (step >= N_MAX_STEP):
+                report = "E: {ep} S: {step} T: {ts} R: {reward}\n".format(
+                    ep=ep,
+                    step=step,
+                    reward=ep_reward,
+                    ts=total_step)
+                log_fp.write(report)
+                log_fp.flush()
+                # print(report)
+                save_steps[t, ep] = step
+                break
 
+step_fn = LOG_PATH + '/steps'
+sio.savemat(step_fn, {'steps': save_steps})
 
-
-
+plt.plot(save_steps.mean(0))
+plt.xlabel('Time slot')
+plt.ylabel('Steps')
+plt.show()
 
 
