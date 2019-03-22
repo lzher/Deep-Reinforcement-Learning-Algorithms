b36bd90f26f0016699e827aad40ec794182fea39
lzhtony <lzhtony@foxmail.com>
2019-03-22 10:58:57 +0800

    change CartPole to pendulum for faster training.

--- PrioritizedExperienceReplay/dqn_base_test.py
+++ PrioritizedExperienceReplay/dqn_base_test.py
@@ -7,21 +7,23 @@ import scipy.io as sio
 import matplotlib.pyplot as plt
 import numpy as np
 
-env = gym.make('CartPole-v1').unwrapped
+env = gym.make('Pendulum-v0').unwrapped
 
 N_TIMES = 5
 N_EP = 300
+N_STEP = 200
 
-N_WARMUP = 200
+N_WARMUP_EP = 20
 N_REPLACE_TARGET = 200
-N_MAX_STEP = 2000
 
 N_MEM = 30000
 N_BATCH = 32
 
-s_dim = 4
-a_dim = 1
-a_val = 2
+s_dim = env.observation_space.shape[0]
+a_dim = env.action_space.shape[0]
+a_bound = env.action_space.high[0]
+a_val = 20
+actions = np.linspace(-a_bound, a_bound, a_val)
 
 now = datetime.datetime.now()
 LOG_PATH = 'logs/R_' + os.path.basename(__file__) + now.strftime('_%Y_%m_%d_%H_%M_%S')
@@ -30,7 +32,7 @@ LOG_FILE = LOG_PATH + '/train.log'
 log_fp = open(LOG_FILE, 'w')
 print(LOG_FILE)
 
-save_steps = np.zeros((N_TIMES, N_EP))
+save_rewards = np.zeros((N_TIMES, N_EP))
 
 dqn = DQN(s_dim, a_val, a_dim)
 mem = ReplayMemory(s_dim, a_dim, N_MEM)
@@ -45,45 +47,38 @@ for t in range(N_TIMES):
         ep_reward = 0
         step = 0
         state = env.reset()
-        while True:
+        for step in range(N_STEP):
             # env.render()
             action = dqn.choose_action(state)
-            state_next, reward, done, _ = env.step(action)
-            
-            xpos, xvel, theta, thetavel = state_next
-            r1 = (env.x_threshold - abs(xpos)) / env.x_threshold - 0.5
-            r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-            reward = r1 + r2
+            act = actions[action]
+            state_next, reward, done, _ = env.step([act])
             
             mem.store_transition(state, state_next, action, reward)
             
-            if total_step > N_WARMUP:
+            if ep > N_WARMUP_EP:
                 dqn.learn(mem, N_BATCH, replacement=(train_step % N_REPLACE_TARGET == 0))
                 train_step += 1
                 
             state = state_next
             ep_reward += reward
-            step += 1
             total_step += 1
             
-            if done or (step >= N_MAX_STEP):
-                report = "E: {ep} S: {step} T: {ts} R: {reward}\n".format(
-                    ep=ep,
-                    step=step,
-                    reward=ep_reward,
-                    ts=total_step)
-                log_fp.write(report)
-                log_fp.flush()
-                # print(report)
-                save_steps[t, ep] = step
-                break
+        report = "E: {ep} T: {ts} R: {reward}\n".format(
+            ep=ep,
+            step=step,
+            reward=ep_reward,
+            ts=total_step)
+        log_fp.write(report)
+        log_fp.flush()
+        # print(report)
+        save_rewards[t, ep] = reward
 
-step_fn = LOG_PATH + '/steps'
-sio.savemat(step_fn, {'steps': save_steps})
+step_fn = LOG_PATH + '/rewards'
+sio.savemat(step_fn, {'rewards': save_rewards})
 
-plt.plot(save_steps.mean(0))
+plt.plot(save_rewards.mean(0))
 plt.xlabel('Time slot')
-plt.ylabel('Steps')
+plt.ylabel('Rewards')
 plt.show()
 
 
