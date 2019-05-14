7cc298c40756f3712359c59c2a572e8d3a3ee913
lzhtony <lzhtony@foxmail.com>
2019-05-14 16:11:01 +0800

    episode reward.

--- HindsightExperienceReplay/flip_ep.py
+++ HindsightExperienceReplay/flip_ep.py
@@ -5,13 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io as sio
 import datetime
+from collections import namedtuple
 
 start_time = datetime.datetime.now().strftime('%m%d%H%M%S')
 print(start_time)
 
 N_BITS = 5
 
-N_TIMES = 50
+N_TIMES = 10
 N_EP = 1000
 N_STEP = 20
 N_WARMUP_EP = 5
@@ -24,6 +25,9 @@ ADIM = 1
 N_ACTION = N_BITS
 
 save_reward = np.zeros((N_TIMES, N_EP))
+save_true_reward = np.zeros((N_TIMES, N_EP))
+
+EpMemory = namedtuple('EpMemory', ['state', 'state_next', 'action', 'reward'])
 
 dqn = DQN(SDIM, N_ACTION, ADIM)
 mem = ReplayMemory(SDIM, ADIM, N_MEM)
@@ -36,6 +40,7 @@ for t in range(N_TIMES):
     for ep in range(N_EP):
         current, target = env.reset()
         state = np.hstack([current, target])
+        ep_mem = []
         for step in range(N_STEP):
             action = dqn.choose_action(state)
             state_next, reward, done, _ = env.step(action)
@@ -43,7 +48,7 @@ for t in range(N_TIMES):
             current, target = state_next
             state_next = np.hstack([current, target])
             
-            mem.store_transition(state, state_next, action, reward)
+            ep_mem.append(EpMemory(state=state, state_next=state_next, action=action, reward=reward))
             
             if ep >= N_WARMUP_EP:
                 dqn.learn(mem, N_BATCH)
@@ -53,10 +58,23 @@ for t in range(N_TIMES):
             if done:
                 break
                 
-        print("T: {t}/{tt} E: {ep} S: {step} R: {reward}".format(t=t, tt=N_TIMES, ep=ep, step=step, reward=reward / (step + 1)))
-        save_reward[t, ep] = reward / (step + 1)
+        ep_reward = 0
+        if done:
+            for e in ep_mem:
+                reward = e.reward + 1
+                mem.store_transition(e.state, e.state_next, e.action, reward)
+                ep_reward += reward
+        else:
+            for e in ep_mem:
+                reward = e.reward - 1
+                mem.store_transition(e.state, e.state_next, e.action, reward)
+                ep_reward += reward
+                
+        print("T: {t}/{tt} E: {ep} S: {step} R: {reward}".format(t=t, tt=N_TIMES, ep=ep, step=step, reward=ep_reward / (step + 1)))
+        save_reward[t, ep] = int(done) / (step + 1)
+        save_true_reward[t, ep] = ep_reward / (step + 1)
     
-sio.savemat('logs/flip_base_%s.mat' % (start_time), {'reward': save_reward})
+sio.savemat('logs/flip_ep_%s.mat' % (start_time), {'reward': save_reward, 'true_reward': save_true_reward})
 end_time = datetime.datetime.now().strftime('%m%d%H%M%S')
 print(start_time, end_time)
     
